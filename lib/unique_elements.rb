e22b7e90ddf6d0c7019b091920a188e1186f3549
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-02 18:40:12 +0000

    Find Unique Elements Done

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,21 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-  
+  occurance_hash = Hash.new
+  arr.each do |a|
+      occurances = 0
+      arr.each do |b|
+          if a == b
+              occurances += 1
+          end
+      end
+      occurance_hash[a] = occurances
+  end
+  unique_values = Array.new
+  occurance_hash.each do |k, v|
+      if v == 1
+          unique_values << k
+      end
+  end
+  unique_values
 end
